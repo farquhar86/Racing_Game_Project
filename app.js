c4032b92ce09073e9e6a7cf709aa96602a308501
bradfarquhar <farquharbradley@gmail.com>
2015-08-24 08:32:45 -0700

    Got my game back to where the game was last week

--- app.js
+++ app.js
@@ -1,41 +1,135 @@
+
+
 // OOP Racing Game example boilerplate code
-
 function Game() {
+  // number of moves needed to win
+  this.numOfMoves = 20;
   //Create a new instance of player 1
-  //this.player1 = ...
+  this.playerOne = new Player(1);
+  this.playerTwo = new Player(2);
 
-  //Do the same for a player 2
-  //this.player2 = ...
-
-  //Create the track
-  //this.track = ...
 }
 
-// `Game.prototype.init` kicks off a new game with a board and two players
-Game.prototype.init = function() {
-  //
+// A starter Player constructor.
+function Player(id) {
+  this.id = id
+  this.count = 0; 
+  this.moves = 20;
+  
 };
 
-// A starter Player constructor.
-function Player(team) {
-  //this.name = ...
-  //this.position = ...
-};
+
+Game.prototype.init = function() {
+  alert("Get ready because there is a Kitty killing everyone in space!!! You need to fly your Space Ship really fast to avoid getting eaten!! Are you ready??");
+
+  var playerOneName = prompt("What is Player Ones Name? To move your ship press the SPACEBAR key repeatedly as fast as you can!!")
+  var playerTwoName = prompt("What is Player Twos Name? To move your ship press the ENTER key repeatedly as fast as you can!!")
+  var _this = this;
+  var startTimer = true
+  var finishTime = true
+  var spaceBar = 32
+  var enter = 13
+  $(document).keyup(function(e) {
+        
+        if (((_this.playerOne.count === 0) || (_this.playerTwo.count === _this.numOfMoves)) && (startTimer === true)) {
+          timeOne = (new Date ($.now()))
+            timerId = window.setInterval(function() {
+              var $clock = $("#total-time").text((($.now()) - timeOne )/1000 ) 
+            })
+            startTimer = false
+
+        } else if (((_this.playerOne.count === _this.numOfMoves) || (_this.playerTwo.count === _this.numOfMoves)) && (finishTime === true)) {
+       
+          timeTwo= ($.now())
+          $("#total-time").text((timeTwo - timeOne)/1000)
+          $('.winner').after("<div class='timeColor'> The Winning Time Is " + ((timeTwo - timeOne)/1000) + " Seconds </div>")
+          stopTimer = true;
+          $('.timer').remove()
+          finishTime = false
+        }
+          
+        if ((e.which == spaceBar) && (_this.playerOne.count < _this.numOfMoves)) {  
+          _this.playerOne.move()    
+        } else if ((e.which == enter) && (_this.playerTwo.count < _this.numOfMoves)) {
+          _this.playerTwo.move();    
+        }; 
+
+        window.setInterval(function(){
+            $('.planet1').animate({"right": "+=10px"}, 'slow')
+          }, 20)
+        window.setInterval(function(){
+            $('.planet2').animate({"right": "+=10px"}, 'fast')
+          }, 20)
+    
+        window.setInterval(function(){
+            $('.catPicture').animate({"left": "+=5px"}, 'fast')
+          }, 20)
+   
+});
+}
+
+Game.prototype.reset = function () {
+  if ((_this.playerOne.count === _this.numOfMoves) || (_this.playerTwo.count === _this.numOfMoves)) {
+    console.log("hello")
+  }
+
+
+}
 
 // Remember: prototypes are shared functions between all game instances
+
 Player.prototype.move = function() {
-  //update player's position
-};
+  
+  if (this.id === 1) {
+    $('.rocketShipOne').animate({"left": "+=20px"}, "fast"); 
+  } else if (this.id === 2){
+    $('.rocketShipTwo').animate({"left": "+=20px"}, "fast");
+  }  
+   this.count++;
+   var stopSpinning = 0;
 
+   if ((this.count === this.moves) && (this.id === 1) && (stopSpinning === 0)) {
+      $(".rocketShipTwo").animate({ transform: 1440, 'width': '5px', 'height': '2px'},  
+            { duration: 700, step: function(now, fx) {  
+                    if (fx.prop == "transform") {  
+                        if (now > 360) { $(this).css({  
+                                '-webkit-transform': 'rotate(' + now + 'deg)',  
+                                '-moz-transform': 'rotate(' + now + 'deg)',     
+                                '-ms-transform': 'rotate(' + now + 'deg)',      
+                                '-o-transform': 'rotate(' + now + 'deg)',       
+                                'transform': 'rotate(' + now + 'deg)',          
+                                'opacity': now / 1440  
+                            })  
+                        }  
+                    }  
+                }  
+            });  
+      stopSpinning = stopSpinning + 1
 
-// A starter Track constructor.
-function Track() {
-  //Tracks the cells of the board instance
-  //this.$cells = ...
+    } else if ((this.count === this.moves) && (this.id === 2) && (stopSpinning === 0)) {
 
-  //Store any other properties that board may have below, such as a reset option
-};
+      $(".rocketShipOne").animate({ transform: 1440, 'width': '5px', 'height': '2px'},  
+            { duration: 700, step: function(now, fx) {  
+                    if (fx.prop == "transform") {  
+                        if (now > 360) { $(this).css({  
+                                '-webkit-transform': 'rotate(' + now + 'deg)',  
+                                '-moz-transform': 'rotate(' + now + 'deg)',     
+                                '-ms-transform': 'rotate(' + now + 'deg)',      
+                                '-o-transform': 'rotate(' + now + 'deg)',       
+                                'transform': 'rotate(' + now + 'deg)',          
+                                'opacity': now / 1440  
+                            })  
+                        }  
+                    }  
+                }  
+            });
+      stopSpinning = stopSpinning + 1
+    } 
+  
+   
+ };
 
-// Start the game!
+$(document).ready(function(){
 var game = new Game();
 game.init();
+})
